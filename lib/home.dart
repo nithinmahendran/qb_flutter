41385ddf6332c9a308513dc270005ef4c7a32d2f
Nithin Mahendran <nithinmahendren@gmail.com>
2020-09-26 23:40:04 +0530

    Final Day basic pages done

--- lib/home.dart
+++ lib/home.dart
@@ -4,39 +4,227 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(30.0),
-            child: Text(
-              "Explore",
-              style: TextStyle(fontSize: 32.0),
+        body: ListView(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 10.0, top: 10.0),
+              child: Text(
+                "Explore",
+                style: TextStyle(fontSize: 32.0),
+              ),
             ),
-          ),
-          Row(
-            children: [
-              Expanded(
-                child: Container(
-                  height: 100.0,
-                  decoration: BoxDecoration(
-                      color: Color(0XFFF67283),
-                      borderRadius: BorderRadius.circular(5.0)),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(Icons.local_car_wash, color: Colors.white),
-                      Text(
-                        "Motors",
-                        style: TextStyle(color: Colors.white),
-                      )
-                    ],
+            Row(
+              children: [
+                Expanded(
+                  //first
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      height: 100.0,
+                      decoration: BoxDecoration(
+                          color: Color(0XFFF67283),
+                          borderRadius: BorderRadius.circular(5.0)),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(Icons.local_car_wash, color: Colors.white),
+                          Text(
+                            "Motors",
+                            style: TextStyle(color: Colors.white),
+                          )
+                        ],
+                      ),
+                    ),
                   ),
                 ),
-              )
-            ],
-          )
-        ],
-      ),
-    );
+                Expanded(
+                  //second
+                  child: Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: Column(
+                      children: [
+                        Container(
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                              color: Color(0XFFF67283),
+                              borderRadius: BorderRadius.circular(5.0)),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(Icons.local_car_wash, color: Colors.white),
+                              Text(
+                                "Motors",
+                                style: TextStyle(color: Colors.white),
+                              )
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          height: 4.0,
+                        ),
+                        Container(
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                              color: Color(0XFFF67283),
+                              borderRadius: BorderRadius.circular(5.0)),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(Icons.local_car_wash, color: Colors.white),
+                              Text(
+                                "Motors",
+                                style: TextStyle(color: Colors.white),
+                              )
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  //second
+                  child: Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: Column(
+                      children: [
+                        Container(
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                              color: Color(0XFFF67283),
+                              borderRadius: BorderRadius.circular(5.0)),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(Icons.local_car_wash, color: Colors.white),
+                              Text(
+                                "Motors",
+                                style: TextStyle(color: Colors.white),
+                              )
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          height: 4.0,
+                        ),
+                        Container(
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                              color: Color(0XFFF67283),
+                              borderRadius: BorderRadius.circular(5.0)),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(Icons.local_car_wash, color: Colors.white),
+                              Text(
+                                "Motors",
+                                style: TextStyle(color: Colors.white),
+                              )
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [Text("Popular Trending"), Text("View All")],
+              ),
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      height: 150,
+                      child: Column(
+                        children: [
+                          Container(
+                            height: 100,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(8),
+                                image: DecorationImage(
+                                    fit: BoxFit.cover,
+                                    image: NetworkImage(
+                                        "https://images.pexels.com/photos/442576/pexels-photo-442576.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"))),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text("Play Station")
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      height: 150,
+                      child: Column(
+                        children: [
+                          Container(
+                            height: 100,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(8),
+                                image: DecorationImage(
+                                    fit: BoxFit.cover,
+                                    image: NetworkImage(
+                                        "https://images.pexels.com/photos/442576/pexels-photo-442576.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"))),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text("Play Station")
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      height: 150,
+                      child: Column(
+                        children: [
+                          Container(
+                            height: 100,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(8),
+                                image: DecorationImage(
+                                    fit: BoxFit.cover,
+                                    image: NetworkImage(
+                                        "https://images.pexels.com/photos/442576/pexels-photo-442576.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"))),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text("Play Station")
+                        ],
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home), title: Text("Home")),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.search), title: Text("Search")),
+            BottomNavigationBarItem(icon: Icon(Icons.tab), title: Text("Tabs")),
+          ],
+        ));
   }
 }
